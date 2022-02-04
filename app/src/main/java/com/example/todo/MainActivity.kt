516c40af0101e86512ccb1f38133c7faa8829ba3
CoalMiner122113 <kolesoto@gmail.com>
2022-02-04 17:00:24 -0600

    Added RecyclerViewer with manual test entries

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listOfTasks.add("laundry")
+        listOfTasks.add("Check 1")
+
+        //Look up recycler view in layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView) as RecyclerView
+        //Create adapter to pass in values
+        val adapter = TaskItemAdapter(listOfTasks)
+        //attach adapter to recycler view to populate itemes
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = (LinearLayoutManager(this))
+
+
+        //Event : User Clicks "Add"
+/*        findViewById<Button>(R.id.button).setOnClickListener{
+            //Code executed when "Add" clicked
+            Log.i("Kole","User clicked Add")
+        }*/
+
+
     }
 }
---
+++ app/src/main/java/com/example/todo/TaskItemAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.todo
+import android.service.autofill.TextValueSanitizer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+//Bride for user input to recycler view
+class TaskItemAdapter(val listOfItems: List<String>) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+    //Provides direct ref to each view within a data item
+    //Caches the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //Stores refs to elements in our layout view
+        val textView: TextView
+
+        init{
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    //involves populating data into the item through the holder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Get Data model based on position
+        val item: String = listOfItems.get(position)
+        //Set item views based on yout views and data model
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+}
